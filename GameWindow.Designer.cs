648e3a033b93ff1e2dfd73253644efe7c28ea283
JonathanLCunnane <jonathan.cunnane@gmail.com>
2022-10-24 15:48:30 +0100

    Basic game implementation complete.

---
+++ GameWindow.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Conway_s_Game_of_Life
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.boardPictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.generationIntervalTimer = new System.Windows.Forms.Timer(this.components);
+            this.stopButton = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.boardPictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // boardPictureBox
+            // 
+            this.boardPictureBox.Location = new System.Drawing.Point(12, 37);
+            this.boardPictureBox.Name = "boardPictureBox";
+            this.boardPictureBox.Size = new System.Drawing.Size(1600, 900);
+            this.boardPictureBox.TabIndex = 0;
+            this.boardPictureBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startButton,
+            this.stopButton});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1634, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startButton
+            // 
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(43, 20);
+            this.startButton.Text = "Start";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // generationIntervalTimer
+            // 
+            this.generationIntervalTimer.Interval = 10;
+            this.generationIntervalTimer.Tick += new System.EventHandler(this.generationIntervalTimer_Tick);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(43, 20);
+            this.stopButton.Text = "Stop";
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1634, 957);
+            this.Controls.Add(this.boardPictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainWindow";
+            this.Text = "Conway\'s Game of Life - Playing";
+            ((System.ComponentModel.ISupportInitialize)(this.boardPictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox boardPictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startButton;
+        private System.Windows.Forms.Timer generationIntervalTimer;
+        private System.Windows.Forms.ToolStripMenuItem stopButton;
+    }
+}
+
---
+++ GameWindow.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conway_s_Game_of_Life
+{
+    public partial class mainWindow : Form
+    {
+        int width = 400;
+        int height = 225;
+        Random rand = new Random();
+        bool[,] board;
+        public mainWindow()
+        {
+            InitializeComponent();
+            board = new bool[height, width];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    board[y, x] = (rand.NextDouble() > 0.95);
+                }
+            }
+            boardPictureBox.Image = getBoardBitmap(board);
+        }
+
+        int getNeighbourCount(bool[,] board, int x, int y)
+        {
+            int count = 0;
+            for (int xOff = -1; xOff < 2; xOff++)
+            {
+                for (int yOff = -1; yOff < 2; yOff++)
+                {
+                    int xCheck = x + xOff;
+                    int yCheck = y + yOff;
+                    if (xCheck == -1 || xCheck == width || yCheck == -1 || yCheck == height)
+                    {
+                        continue;
+                    }
+                    if (board[yCheck, xCheck])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        Bitmap getBoardBitmap(bool[,] board)
+        {
+            float bmpWidth = boardPictureBox.Width;
+            float bmpHeight = boardPictureBox.Height;
+            float cellWidth = bmpWidth / width;
+            float cellHeight = bmpHeight / height;
+            Bitmap boardBitmap = new Bitmap(
+                (int)bmpWidth,
+                (int)bmpHeight
+                );
+            using (Graphics boardGraphics = Graphics.FromImage(boardBitmap))
+            {
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        Brush currBrush;
+                        if (board[y, x])
+                        {
+                            currBrush = Brushes.Black;
+                        }
+                        else
+                        {
+                            currBrush = Brushes.White;
+                        }
+                        boardGraphics.FillRectangle(
+                            currBrush,
+                            cellWidth*x,
+                            cellHeight*y,
+                            cellWidth,
+                            cellHeight
+                            );
+                    }
+                }
+            }
+            return boardBitmap;
+        }
+
+        IEnumerator<Bitmap> boardGenerations(bool[,] startBoard)
+        {
+            while (true)
+            {
+                bool[,] newBoard = new bool[height, width];
+                // Game logic below
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int currNeighbours = getNeighbourCount(startBoard, x, y);
+                        // If alive
+                        if (startBoard[y, x])
+                        {
+                            if (currNeighbours == 2 || currNeighbours == 3)
+                            {
+                                newBoard[y, x] = true;
+                            }
+                            else
+                            {
+                                newBoard[y, x] = false;
+                            }
+                        }
+                        // If dead
+                        else
+                        {
+                            if (currNeighbours == 3)
+                            {
+                                newBoard[y, x] = true;
+                            }
+                            else
+                            {
+                                newBoard[y, x] = false;
+                            }
+                        }
+
+                    }
+                }
+                startBoard = newBoard;
+                yield return getBoardBitmap(startBoard);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            generationIntervalTimer.Start();
+            generationIntervalTimer.Tag = boardGenerations(board);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            generationIntervalTimer.Stop();
+            ((IEnumerator<Bitmap>)(generationIntervalTimer.Tag)).Dispose();
+
+            boardPictureBox.Image = getBoardBitmap(board);
+        }
+
+        private void generationIntervalTimer_Tick(object sender, EventArgs e)
+        {
+            ((IEnumerator<Bitmap>)(generationIntervalTimer.Tag)).MoveNext();
+            boardPictureBox.Image = ((IEnumerator<Bitmap>)(generationIntervalTimer.Tag)).Current;
+        }
+    }
+}
